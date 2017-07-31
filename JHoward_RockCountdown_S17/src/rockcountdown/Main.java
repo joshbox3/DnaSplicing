31d25db5ca73eac973970dd10e09fe90b39cdb91
joshbox3 <jjhowardsd@gmail.com>
2017-07-31 13:12:30 -0700

    Second Commit!

--- JHoward_RockCountdown_S17/src/rockcountdown/Main.java
+++ JHoward_RockCountdown_S17/src/rockcountdown/Main.java
@@ -8,16 +8,22 @@ import org.apache.commons.io.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		// This is my second commit!
+		// This will be shown only after I commit!
+
 		Scanner scan = new Scanner(System.in); // scan in filename
 		String input = scan.nextLine(); // get filename from input
 		File file = new File(input); // the input is defined as a file
-		String wholeFile = FileUtils.readFileToString(file); // the whole file is written to a string
+		String wholeFile = FileUtils.readFileToString(file); // the whole file
+																// is written to
+																// a string
 		String[] songs = wholeFile.split("\r\n"); //
 		ArrayList<Song> list = new ArrayList<Song>();
 		for (int i = 0; i < songs.length; i++) {
 			list.add(new Song(songs[i]));
 		}
-		StringBuilder outputText = new StringBuilder(); // this will hold the output text
+		StringBuilder outputText = new StringBuilder(); // this will hold the
+														// output text
 		for (int i = list.size() - 1; i >= 0; i--) {
 			outputText.append(list.get(i).getRank());
 			outputText.append("\t");
